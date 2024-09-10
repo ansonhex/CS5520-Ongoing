fca8d08f476b62a1a998404efd954f626f3f540f
ansonhe97 <ansonhe1997@gmail.com>
2024-09-10 14:45:22 -0700

    practice usestate

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import Header from "./components/Header";
+import Input from "./components/Input";
 
 export default function App() {
   const appName = "AnsonHe App";
+
   return (
     <View style={styles.container}>
       <Header name={appName} />
+      <Input />
       <StatusBar style="auto" />
     </View>
   );
