438cce434e2824a36a2ba1e057bd4ee9f9f6daec
ansonhe97 <ansonhe1997@gmail.com>
2024-09-10 13:48:08 -0700

    added appname

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const appName = "AnsonHe App"
   return (
     <View style={styles.container}>
-      <Text>Welcome!</Text>
+      <Text>Welcome to my {appName}!</Text>
       <StatusBar style="auto" />
     </View>
   );
