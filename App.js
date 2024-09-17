df8312b59cfd3d4e8d894ef09931d65641611317
ansonhex <ansonhe1997@gmail.com>
2024-09-17 14:25:54 -0700

    added modal

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import Header from "./components/Header";
 import Input from "./components/Input";
+import React, { useState } from "react";
 
 export default function App() {
   const appName = "AnsonHe App";
-  const [data, setData] = setState("");
+  const [data, setData] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
   
   const handleInputData = (UserData) => {
     console.log(UserData);
     setData(UserData);
+    setIsModalVisible(false);
   }
 
   return (
     <View style={styles.container}>
       <Header name={appName} />
-      <Input autoFocus={true} inputData={handleInputData} />
+      <Input autoFocus={true} inputData={handleInputData} modal={isModalVisible} />
       <StatusBar style="auto" />
       <Text>{data}</Text>
+      <Button title="Add a goal" onPress={() => setIsModalVisible(true)} />
     </View>
   );
 }
--- components/Input.js
+++ components/Input.js
@@ -1,7 +1,7 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Modal, StyleSheet, Button } from "react-native";
 import React, { useState } from "react";
 
-export default function Input({ autoFocus, inputData }) {
+export default function Input({ autoFocus, inputData, modal }) {
   const [text, setText] = useState("");
   const [isFocused, setIsFocused] = useState(false);
 
@@ -15,37 +15,47 @@ export default function Input({ autoFocus, inputData }) {
   const handleConfirm = () => {
     // logs users input to console log
     inputData(text);
-  }
-
+  };
 
   return (
-    <View>
-      <TextInput
-        placeholder="Type here!"
-        onChangeText={handleChangeText}
-        value={text}
-        autoCorrect={true}
-        style={{ borderBottomWidth: 1, borderBottomColor: "purple" }}
-        autoFocus={autoFocus}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-      />
+    <Modal animationType="slide" visible={modal}>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Type here!"
+          onChangeText={handleChangeText}
+          value={text}
+          autoCorrect={true}
+          style={{ borderBottomWidth: 1, borderBottomColor: "purple" }}
+          autoFocus={autoFocus}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
 
-      {/* <Text>Typed text: {text}</Text> */}
+        {/* <Text>Typed text: {text}</Text> */}
 
-      {/* Character count */}
-      {isFocused && text.length > 0 && <Text>Count: {text.length}</Text>}
+        {/* Character count */}
+        {isFocused && text.length > 0 && <Text>Count: {text.length}</Text>}
 
-      {/* Message after blur or submit */}
-      {!isFocused && (text || "").length > 0 && (
-        <Text>
-          {text.length >= 3
-            ? "Thank you"
-            : "Please type more than 3 characters"}
-        </Text>
-      )}
+        {/* Message after blur or submit */}
+        {!isFocused && (text || "").length > 0 && (
+          <Text>
+            {text.length >= 3
+              ? "Thank you"
+              : "Please type more than 3 characters"}
+          </Text>
+        )}
 
-      <Button title="Confirm" onPress={handleConfirm} />
-    </View>
+        <Button title="Confirm" onPress={handleConfirm} />
+      </View>
+    </Modal>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
