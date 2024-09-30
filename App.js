60ad5765a5a283fb6a37a66fe40caed093a3250e
ansonhex <ansonhe1997@gmail.com>
2024-09-30 00:08:16 -0700

    added dynamic params

--- App.js
+++ App.js
@@ -1,29 +1,46 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import React from 'react'
-import Home from './components/Home'
-import GoalDetails from './components/GoalDetails'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import Home from "./components/Home";
+import GoalDetails from "./components/GoalDetails";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{
-          title: 'My Goals',
-          headerStyle: {
-            backgroundColor: '#ccc',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}/>
-        <Stack.Screen name="Details" component={GoalDetails} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: "My Goals",
+            headerStyle: {
+              backgroundColor: "#ccc",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={GoalDetails}
+          options={({ route }) => ({
+            title: route.params?.goal?.text,
+            headerStyle: {
+              backgroundColor: "#ccc",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default App
+export default App;
