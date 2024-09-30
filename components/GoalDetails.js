0f0d90528aea0f1646aafa478a4532ccaf79f811
ansonhex <ansonhe1997@gmail.com>
2024-09-29 23:58:07 -0700

    added more details

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -1,20 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
 
-const GoalDetails = ({ route }) => {
-
-  const { goal } = route.params;
+const GoalDetails = ({ route, navigation }) => {
+  const { goal, showMoreDetails } = route.params;
   console.log(goal);
 
   return (
     <View>
-      <Text style={styles.title}>Goal Details</Text>
-      <Text style={styles.text}>Goal ID: {goal.id}</Text>
-      <Text style={styles.text}>Goal Text: {goal.text}</Text>
+      {showMoreDetails ? (
+        <Text style={styles.text}>More details</Text>
+      ) : (
+        <>
+          <Text style={styles.title}>Goal Details</Text>
+          <Text style={styles.text}>Goal ID: {goal.id}</Text>
+          <Text style={styles.text}>Goal Text: {goal.text}</Text>
+        </>
+      )}
+
+      <Button
+        title="More Details"
+        onPress={() =>
+          navigation.push("Details", { goal, showMoreDetails: true })
+        }
+      />
     </View>
-  )
-}
+  );
+};
 
-export default GoalDetails
+export default GoalDetails;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
