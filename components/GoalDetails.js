a9e781bf54bc6e4b29d8a4b7a45f1f88fcc5d3cd
ansonhex <ansonhe1997@gmail.com>
2024-10-01 14:53:29 -0700

    fix bug

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -2,25 +2,26 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
 
 const GoalDetails = ({ route, navigation }) => {
-  const { goal, showMoreDetails } = route.params;
+  const { goal } = route.params || {};
   console.log(goal);
 
   return (
     <View>
-      {showMoreDetails ? (
-        <Text style={styles.text}>More details</Text>
-      ) : (
+      {goal?.id ? (
         <>
           <Text style={styles.title}>Goal Details</Text>
           <Text style={styles.text}>Goal ID: {goal.id}</Text>
           <Text style={styles.text}>Goal Text: {goal.text}</Text>
         </>
+      ) : 
+      (
+        <Text style={styles.text}>More details</Text>
       )}
 
       <Button
         title="More Details"
         onPress={() =>
-          navigation.push("Details", { goal, showMoreDetails: true })
+          navigation.push("Details")
         }
       />
     </View>
