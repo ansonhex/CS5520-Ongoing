a33b491bc0076629bd4c359b82ac62082fea049c
ansonhex <ansonhe1997@gmail.com>
2024-10-22 15:05:07 -0700

    update and fix bug

--- components/GoalUsers.js
+++ components/GoalUsers.js
@@ -1,38 +1,59 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import React, { useState, useEffect } from "react";
-import { writeToDB } from "../firebase/firestoreHelper";
+import { getAllDocuments, writeToDB } from "../firebase/firestoreHelper";
 
 export default function GoalUsers({ goalId }) {
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
     // fetch data
     const fetchData = async () => {
       try {
+        const dataFromDb = await getAllDocuments(`Goals/${goalId}/Users`);
+        console.log("dataFromDb: ", dataFromDb);
+
+        if (dataFromDb.length > 0) {
+          setUsers(
+            dataFromDb.map((user) => {
+              return user;
+            })
+          );
+          return;
+        }
+
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/users"
         );
         if (!response.ok) {
           throw new Error(`Http error: ${response.status}`);
         }
+
         const data = await response.json();
-        data.forEach((user) => writeToDB(`Goals/${goalId}/Users`, user));
-        setUsers(data.map((user) => user.name));
-        console.log(users);
+
+        // Write the fetched data to Firestore
+        await Promise.all(
+          data.map(async (user) => {
+            await writeToDB(`Goals/${goalId}/Users`, user);
+          })
+        );
+
+        // Set the fetched users in state
+        setUsers(data);
       } catch (error) {
         console.error(error);
       }
     };
 
-    // call the function
     fetchData();
-  }, []);
+  }, [goalId]);
+
   return (
     <View>
       {/* Flatlist of users */}
       <FlatList
         data={users}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => <Text>{item}</Text>}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
       />
     </View>
   );
