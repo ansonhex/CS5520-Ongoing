c4452800637d62a1dea1eeca063b8ccd41e4dbff
ansonhex <ansonhe1997@gmail.com>
2024-11-11 22:08:30 -0800

    added permission

--- components/LocationManager.js
+++ components/LocationManager.js
@@ -3,7 +3,26 @@ import { View, Button, Alert } from "react-native";
 import * as Location from "expo-location";
 
 const LocationManager = () => {
+  const [response, requestPermission] = Location.useForegroundPermissions();
+
+  const verifyLocation = async () => {
+    if (response?.granted) {
+      return true;
+    }
+
+    const res = await requestPermission();
+    return res.granted;
+  };
+
   const locateUserHandler = async () => {
+    const hasPermission = await verifyLocation();
+    if (!hasPermission) {
+      Alert.alert(
+        "Location Permission",
+        "Please enable location services for this app."
+      );
+      return;
+    }
     try {
       const location = await Location.getCurrentPositionAsync();
       Alert.alert(
