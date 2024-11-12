edf117e678efe92fe95f3e08f28b039b32e029fb
ansonhex <ansonhe1997@gmail.com>
2024-11-12 00:02:26 -0800

    added dynamic map

--- components/LocationManager.js
+++ components/LocationManager.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { View, Button, Alert, Image } from "react-native";
 import * as Location from "expo-location";
+import { useNavigation } from "@react-navigation/native";
 
 const LocationManager = () => {
+  const navigation = useNavigation();
   const mapsApiKey = process.env.EXPO_PUBLIC_mapsApiKey;
   const [location, setLocation] = useState(null);
   const [response, requestPermission] = Location.useForegroundPermissions();
@@ -30,6 +32,7 @@ const LocationManager = () => {
         latitude: locationData.coords.latitude,
         longitude: locationData.coords.longitude,
       });
+      console.log(locationData);
     } catch (err) {
       console.error("Failed", err);
       Alert.alert("Failed", "Failed to get location. Please try again.");
@@ -49,6 +52,19 @@ const LocationManager = () => {
           style={{ width: 400, height: 200, marginTop: 10 }}
         />
       )}
+      <Button
+        title="Open Map"
+        onPress={() => {
+          if (location) {
+            navigation.navigate("Map", { location });
+          } else {
+            Alert.alert(
+              "Location not set",
+              "Please locate your position first."
+            );
+          }
+        }}
+      />
     </View>
   );
 };
---
+++ components/Map.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
+
+const Map = ({ route }) => {
+  const { location } = route.params || {
+    latitude: 37.78825,
+    longitude: -122.4324,
+  }; // Default location
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default Map;
