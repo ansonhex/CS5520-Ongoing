0a4f549a05fb7ee9ea037d481ef53c1478afc3f7
ansonhex <ansonhe1997@gmail.com>
2024-11-11 22:22:56 -0800

    added static map

--- components/LocationManager.js
+++ components/LocationManager.js
@@ -1,15 +1,16 @@
-import React from "react";
-import { View, Button, Alert } from "react-native";
+import React, { useState } from "react";
+import { View, Button, Alert, Image } from "react-native";
 import * as Location from "expo-location";
 
 const LocationManager = () => {
+  const mapsApiKey = process.env.EXPO_PUBLIC_mapsApiKey;
+  const [location, setLocation] = useState(null);
   const [response, requestPermission] = Location.useForegroundPermissions();
 
   const verifyLocation = async () => {
     if (response?.granted) {
       return true;
     }
-
     const res = await requestPermission();
     return res.granted;
   };
@@ -24,20 +25,30 @@ const LocationManager = () => {
       return;
     }
     try {
-      const location = await Location.getCurrentPositionAsync();
-      Alert.alert(
-        "Location",
-        `Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}`
-      );
+      const locationData = await Location.getCurrentPositionAsync();
+      setLocation({
+        latitude: locationData.coords.latitude,
+        longitude: locationData.coords.longitude,
+      });
     } catch (err) {
       console.error("Failed", err);
       Alert.alert("Failed", "Failed to get location. Please try again.");
     }
   };
 
+  const mapUrl = location
+    ? `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${location.latitude},${location.longitude}&key=${mapsApiKey}`
+    : null;
+
   return (
     <View>
-      <Button title="Locate Me" onPress={locateUserHandler} />
+      <Button title="Find My Location" onPress={locateUserHandler} />
+      {location && (
+        <Image
+          source={{ uri: mapUrl }}
+          style={{ width: 400, height: 200, marginTop: 10 }}
+        />
+      )}
     </View>
   );
 };
